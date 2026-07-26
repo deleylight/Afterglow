218db065d48c617e336006c175460c006abe4d57
huanlu <100133765+huanlu233@users.noreply.github.com>
2026-07-26 13:25:34 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    // include_modules!() 已经把 MainWindow 生成并引入进来了
     let main_window = MainWindow::new()?;
-
-    // 运行窗口事件循环
     main_window.run()
 }
